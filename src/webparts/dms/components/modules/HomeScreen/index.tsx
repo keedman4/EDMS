a109af9bba331f0640db78e6494ca5d4eb29a7a1
Gbenga Fagbemi <gbengaobafemi1@gmail.com>
2024-01-19 18:25:32 +0100

    ✨feature: creation of tree hook and implementation with fluent UI

--- src/webparts/dms/components/modules/HomeScreen/index.tsx
+++ src/webparts/dms/components/modules/HomeScreen/index.tsx
@@ -1,28 +1,28 @@
 import * as React from "react";
 import { useDocument } from "office-ui-fabric-react";
-import styles from '../../Dms.module.scss';
+import styles from "../../Dms.module.scss";
 
-import TreeView from '../treeview/index';
-import {DetailsListDocuments} from '../ListItems/DetailListShimmer';
-import {CommandBarMenu} from '../CommandBar/index';
-import DocumentDetails from '../Details/Details';
+import TreeView from "../treeview/index";
+import { DetailsListDocuments } from "../ListItems/DetailListShimmer";
+import { CommandBarMenu } from "../CommandBar/index";
+import DocumentDetails from "../Details/Details";
 
-const HomeScreen = () => {   
-    return (
+const HomeScreen = () => {
+  return (
     <div className={`${styles.dms} ms-Grid`} dir="ltr">
-    {/* <div className="ms-Grid-row"> */}
-    <div className="ms-Grid-col ms-sm12 ms-lg4">
-        <TreeView />
-      </div>
-      <div className="ms-Grid-col ms-sm12 ms-lg4">
+      {/* <div className="ms-Grid-row"> */}
+      {/* <div className="ms-Grid-col ms-sm12 ms-lg4"> */}
+      <TreeView />
+      {/* </div> */}
+      {/* <div className="ms-Grid-col ms-sm12 ms-lg4">
         <DetailsListDocuments/>
       </div>
       <div className="ms-Grid-col ms-sm12 ms-lg4 ms-xl1">
         <DocumentDetails/>
-      </div>
-    {/* </div> */}
-  </div>
-);
+      </div> */}
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default HomeScreen;
--- src/webparts/dms/components/modules/treeview/func.ts
+++ src/webparts/dms/components/modules/treeview/func.ts
@@ -1,85 +1,139 @@
-import {sp} from '@pnp/sp';
-import { uniqueId } from 'lodash';
+import { sp } from "@pnp/sp";
+import { uniqueId } from "lodash";
 
-export async function useDocumentLibrary(){
-    try {
-        const data = await sp.web.folders();
-        
-        const filterData = data?.map(({Name, UniqueId, ServerRelativeUrl}) => ({
-            Name,
-            UniqueId,
-            ServerRelativeUrl
-        }));
+export async function useFetchFolders() {
+  try {
+    const fetchFolderDataRecursive = async (folder) => {
+      const folderItem = await sp.web
+        .getFolderById(folder.UniqueId)
+        .folders.get();
 
-        const getChildrenSubFolders = async(folder) =>{
-            const folderItem = await sp.web
-            .getFolderById(folder?.UniqueId)
-            .folders.get();
+      const fetchSubfolderContentsRecursive = async (subFolder) => {
+        const subfolderItem = await sp.web
+          .getFolderById(subFolder.UniqueId)
+          .folders.get();
 
-            const fileItem = await sp.web.getFolderById(folder?.UniqueId).files.get();
-
-        const filterSubFolder = folderItem?.map(
-            ({ Name, UniqueId, ServerRelativeUrl }) => ({
-                Name,
-                UniqueId,
-                ServerRelativeUrl
-            })
+        const visibleSubFolder = await Promise.all(
+          subfolderItem
+            .filter(
+              (innerSubFolder) =>
+                !innerSubFolder.Name.startsWith("_") &&
+                innerSubFolder.Name !== "Forms" &&
+                innerSubFolder.Name !== "AllItems"
+            )
+            .map(fetchSubfolderContentsRecursive)
         );
 
-        const filterSubFile = fileItem?.map(
-            ({ Name, UniqueId, ServerRelativeUrl }) => ({
-                Name,
-                UniqueId,
-                ServerRelativeUrl
-            })
-        );
-            return {
-                Name: folder?.Name,
-                UniqueId: folder?.UniqueId,
-                ServerRelativeUrl: folder?.ServerRelativeUrl,
-                subfolders: filterSubFolder,
-                files: filterSubFile
-            };
-        }
-
-        const getSubFolders = async(folder) => {
-               const folderItem = await sp.web
-            .getFolderById(folder?.UniqueId)
-            .folders.get();
-
-            const fileItem = await sp.web.getFolderById(folder?.UniqueId).files.get();
-
-        const filterSubFolder = folderItem?.map(
-            ({ Name, UniqueId, ServerRelativeUrl }) => ({
-                Name,
-                UniqueId,
-                ServerRelativeUrl
-            })
-        );
-
-        const filterSubFile = fileItem?.map(
-            ({ Name, UniqueId, ServerRelativeUrl }) => ({
-                Name,
-                UniqueId,
-                ServerRelativeUrl
-            })
-        );
-
-        const folderLoop = await Promise.all(filterSubFolder.map(getChildrenSubFolders));
-
-            return {
-                Name: folder?.Name,
-                UniqueId: folder?.UniqueId,
-                ServerRelativeUrl: folder?.ServerRelativeUrl,
-                subfolders: folderLoop,//filterSubFolder,
-                files: filterSubFile
-            };            
+        return {
+          name: subFolder.Name,
+          key: subFolder?.UniqueId,
+          url: `/${subFolder?.UniqueId}`,
+          TimeCreated: subFolder.TimeCreated,
+          TimeLastModified: subFolder.TimeLastModified,
+          UniqueId: subFolder.UniqueId,
+          ServerRelativeUrl: subFolder.ServerRelativeUrl,
+          ItemCount: subFolder.ItemCount,
+          links: visibleSubFolder,
+          //   iconProps: {
+          //     iconName: "DocLibrary",
+          //     styles: {
+          //       root: {
+          //         fontSize: 20,
+          //         color: "#106ebe",
+          //       },
+          //     },
+          //   },
         };
-    
-    const allFolders = await Promise.all(filterData.map(getSubFolders));
+      };
 
-    return allFolders;
-    } catch (error) {
-        throw error(error);
-    }
+      const visibleFolders = await Promise.all(
+        folderItem
+          .filter(
+            (subFolder) =>
+              !subFolder.Name.startsWith("_") &&
+              subFolder.Name !== "Forms" &&
+              subFolder.Name !== "AllItems"
+          )
+          .map(fetchSubfolderContentsRecursive)
+      );
+
+      return {
+        name: folder?.Name,
+        key: folder?.UniqueId,
+        url: `/${folder?.UniqueId}`,
+        ItemCount: folder?.ItemCount,
+        TimeCreated: folder?.TimeCreated,
+        TimeLastModified: folder?.TimeLastModified,
+        UniqueId: folder?.UniqueId,
+        ServerRelativeUrl: folder?.ServerRelativeUrl,
+        links: visibleFolders,
+        // iconProps: {
+        //   iconName: "DocLibrary",
+        //   styles: {
+        //     root: {
+        //       fontSize: 20,
+        //       color: "#106ebe",
+        //     },
+        //   },
+        // },
+      };
+    };
+
+    const webFolders = await sp.web.folders();
+
+    const filteredWebFolders = webFolders.filter(
+      (folder) =>
+        !folder.Name.startsWith("_") &&
+        folder.Name !== "Forms" &&
+        folder.Name !== "AllItems"
+    );
+
+    const all = await Promise.all(
+      filteredWebFolders.map(fetchFolderDataRecursive)
+    );
+
+    return all;
+  } catch (error) {
+    console.error("Error fetching folders data:", error);
+    throw error;
+  }
+}
+
+export async function useFolderData(folder) {
+  const single = await sp.web.getFolderById(folder).get();
+  const folderItem = await sp.web.getFolderById(folder).folders.get();
+  const visibleFolders = folderItem.filter(
+    (subFolder) =>
+      !subFolder.Name.startsWith("_") &&
+      subFolder.Name !== "Forms" &&
+      subFolder.Name !== "AllItems"
+  );
+
+  const files = await sp.web.getFolderById(folder).files.get();
+
+  const formattedFiles = files.map((file) => ({
+    ...file,
+    TimeCreated: new Date(file.TimeCreated).toLocaleDateString(),
+    TimeLastModified: new Date(file.TimeLastModified).toLocaleDateString(),
+  }));
+
+  return {
+    Name: single?.Name,
+    name: single?.Name,
+    key: folder,
+    url: `/${folder}`,
+    UniqueId: folder,
+    ServerRelativeUrl: single?.ServerRelativeUrl,
+    folders: visibleFolders,
+    files: formattedFiles,
+    iconProps: {
+      iconName: "DocLibrary",
+      styles: {
+        root: {
+          fontSize: 20,
+          color: "#106ebe",
+        },
+      },
+    },
+  };
 }
